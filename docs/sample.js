0976de86680ab7f20b6ff36539b4dfe643afdcde
horen5x <48595788+horen5x@users.noreply.github.com>
2021-03-04 23:29:30 +0900

    Update sample.js

--- docs/sample.js
+++ docs/sample.js
@@ -2,6 +2,7 @@ dd = new Date();
 document.write(dd.toLocaleString());
 window.addEventListener('load',function(){
   setInterval(function(){
+    dd = new Date();
     document.getElementById("T1").innerHTML = dd.toLocaleString();
   },1000);
 })
